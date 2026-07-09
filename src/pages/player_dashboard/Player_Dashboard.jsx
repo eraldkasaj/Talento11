684acd06b35038a4a64a998e320fdb801a2868fc
aldikasa <eraldkasaj14@gmail.com>
2026-07-09 20:43:14 +0200

    Add scout dashboard, image upload and user profile improvements

--- src/pages/player_dashboard/Player_Dashboard.jsx
+++ src/pages/player_dashboard/Player_Dashboard.jsx
@@ -1,19 +1,32 @@
 import "./Player_Dashboard.css";
+
 import { useEffect,useState } from "react";
+
 import { auth,db } from "../../firebase/firebase";
+
 import { ref,get } from "firebase/database";
+
 import { useNavigate } from "react-router-dom";
+
+import { signOut } from "firebase/auth";
+
+
 
 function Player_Dashboard(){
 
+
 const navigate = useNavigate();
+
 
 const [userData,setUserData] = useState(null);
 
 
+
 useEffect(()=>{
 
+
 const getUserData = async()=>{
+
 
 const user = auth.currentUser;
 
@@ -42,6 +55,7 @@ setUserData(snapshot.val());
 }
 
 
+
 getUserData();
 
 
@@ -49,7 +63,41 @@ getUserData();
 
 
 
+
+
+const logout = async()=>{
+
+
+try{
+
+
+await signOut(auth);
+
+
+navigate("/login");
+
+
+}
+
+
+catch(error){
+
+
+console.log(error.message);
+
+
+}
+
+
+}
+
+
+
+
+
+
 return(
+
 
 <section className="player-dashboard">
 
@@ -57,19 +105,39 @@ return(
 <h1>Paneli i Lojtarit</h1>
 
 
+
 {
 
-userData &&
+
+userData && (
+
 
 <h2>
+
 Mirësevjen {userData.name} {userData.surname}
+
 </h2>
+
+
+)
 
 }
 
 
 
+<button onClick={logout}>
+
+Dil
+
+</button>
+
+
+
+
+
 <div className="dashboard-cards">
+
+
 
 
 
@@ -78,57 +146,104 @@ className="dashboard-card"
 onClick={()=>navigate("/my-profile")}
 >
 
+
 <h3>👤 Profili im</h3>
 
+
 <p>
-Menaxho të dhënat personale.
+
+Shiko profilin tënd.
+
 </p>
 
+
 </div>
+
+
+
+
 
 
 <div 
 className="dashboard-card"
 onClick={()=>navigate("/statistics")}
 >
+
+
 <h3>📊 Statistics</h3>
+
+
 <p>
+
 Shiko statistikat.
+
 </p>
+
+
 </div>
 
 
-<div
+
+
+
+
+<div 
 className="dashboard-card"
 onClick={()=>navigate("/highlights")}
-><h3>🎥 Highlights</h3>
+>
+
+
+<h3>🎥 Highlights</h3>
+
+
 <p>
+
 Menaxho videot.
+
 </p>
+
+
 </div>
 
 
 
-<div className="dashboard-card">
+
+
+
+
+<div 
+className="dashboard-card"
+onClick={()=>navigate("/settings")}
+>
+
 
 <h3>⚙️ Cilësimet</h3>
 
+
 <p>
+
 Menaxho llogarinë.
+
 </p>
 
-</div>
-
-
 
 </div>
+
+
+
+
+</div>
+
 
 
 </section>
 
+
 )
 
+
 }
+
 
 
 export default Player_Dashboard;
